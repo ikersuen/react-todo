c6759f0f963b88a9de80e96ba40e264587cff036
Iker Suen <suenik@oocl.com>
2018-12-05 17:30:19 +0800

    [iker] feat: add fetch api to todoinputContainer

--- src/containers/TodoInputContainer.js
+++ src/containers/TodoInputContainer.js
@@ -2,11 +2,37 @@ import React, { Component } from 'react'
 import { connect } from "react-redux";
 import TodoInput from '../components/TodoInput'
 
-const mapDispatchToProps = {
-    addNewTodo: newTodo => ({
-        type: "ADD_NEW_TODO",
-      payload: newTodo
-    })
-}
+const mapDispatchToProps = (dispatch) => ({
+    addNewTodo: newTodo => {
+        const newTodoItem = {
+            content: newTodo,
+            status: "active"
+        }
+        fetch("http://localhost:8080/api/todos", {
+            method: 'POST', 
+            headers: new Headers({'Content-Type': 'application/json'}), 
+            mode: 'cors', 
+            body: JSON.stringify(newTodoItem)
+        })
+        .then(res => res.json())
+        .then(({id, status, content}) => {
+            dispatch({
+                type: "ADD_NEW_TODO",
+                payload: {id, status, content}
+            })
+        })
+        // .then(res => {
+        //     dispatch({
+        //         type: "ADD_NEW_TODO",
+        //         payload: {
+        //             id: res.id,
+        //             content: res.content,
+        //             status: res.status
+        //         }
+        //     })
+        // })
+        
+    }
+})
 
 export default connect(null, mapDispatchToProps)(TodoInput)
